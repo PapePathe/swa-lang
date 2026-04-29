f064629c3e5cecb5e5509da814898827bef0e57f
pathe <pathe.sene@gmail.com>
2026-04-29 16:17:38 +0000

    feat: test codegen for arithmetic expressions

--- tests/compiler_tests.cpp
+++ tests/compiler_tests.cpp
@@ -35,6 +35,126 @@ TEST_F(JITOutputTest, ParseMinimalProgram) {
   ASSERT_EQ(output, "Salam");
 }
 
+TEST_F(JITOutputTest, Print) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("10 = %d", 10); 
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "10 = 10");
+}
+
+TEST_F(JITOutputTest, Print1) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("10 - 3 - 2 = %d", 10 - 3 - 2);
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "10 - 3 - 2 = 5");
+}
+
+TEST_F(JITOutputTest, Print2) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("16 / 4 / 2 = %d", 16 / 4 / 2);
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "16 / 4 / 2 = 2");
+}
+
+TEST_F(JITOutputTest, Print3) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("5 + 3 * 2 - 8 / 4 = %d", 5 + 3 * 2 - 8 / 4);
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "5 + 3 * 2 - 8 / 4 = 9");
+}
+
+TEST_F(JITOutputTest, Print4) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("7 / 2 = %d", 7/2);
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "7 / 2 = 3");
+}
+
+TEST_F(JITOutputTest, Print5) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("12 * 3 / 4 = %d", 12 * 3 / 4);
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "12 * 3 / 4 = 9");
+}
+
+TEST_F(JITOutputTest, Print6) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("5 - 3 + 2 = %d", 5 - 3 + 2);
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "5 - 3 + 2 = 4");
+}
+
+TEST_F(JITOutputTest, Print7) {
+  std::string output = runAndCapture([&]() {
+    std::string program = R"(
+      dialect:english;
+      start() int {
+        print("((2+3)*(4-1))/(5-2) = %d", ((2 + 3) * (4 - 1)) / (5 - 2));
+      }
+    )";
+    SwaCompiler swa;
+    swa.Run(program);
+  });
+
+  ASSERT_EQ(output, "((2+3)*(4-1))/(5-2) = 5");
+}
+
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest();
   int _ = RUN_ALL_TESTS();
