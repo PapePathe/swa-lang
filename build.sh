8f4ac0dfc4ce79ed5b5d25d3ea8bd06d0140058c
pathe <pathe.sene@gmail.com>
2026-05-06 23:24:59 +0000

    feat: setup cpack in cmake

--- build.sh
+++ build.sh
@@ -5,10 +5,17 @@ set -euxo pipefail
  LLVM_INSTALL_DIR=/usr/include/llvm/ 
  
  cmake -DLLVM_INSTALL_DIR=$LLVM_INSTALL_DIR \
+       -DCMAKE_C_COMPILER=clang-21 \
+       -DCMAKE_CXX_COMPILER=clang++ \
        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+       -DLLVM_DIR=/usr/lib/llvm-21/lib/cmake/llvm \
        -G Ninja -B build .
  
  cd build
  cmake --build .
  
  ./compiler_tests
+
+
+cpack -G TGZ 
+cpack -G DEB --config CPackConfig.cmake
