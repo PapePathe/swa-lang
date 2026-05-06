9cdfe05e700c310c1b589c731e8d2ac67927ee3c
pathe <pathe.sene@gmail.com>
2026-05-06 06:02:52 +0000

    feat: add a build command to generate final executable

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,13 +1,84 @@
 #include <compiler/compiler.h>
+#include <cstdlib>
 #include <llvm/Bitcode/BitcodeWriter.h>
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/MC/TargetRegistry.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/StandardInstrumentations.h>
+#include <llvm/Support/CodeGen.h>
+#include <llvm/Support/FileSystem.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/TargetParser/Host.h>
+#include <llvm/TargetParser/Triple.h>
+#include <optional>
 #include <string>
+#include <utility>
 #include <vector>
+
+void emitObjectFile(llvm::Module *TheModule,
+                    llvm::TargetMachine *TargetMachine) {
+  auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+  TheModule->setTargetTriple(llvm::Triple(TargetTriple));
+  TheModule->setDataLayout(TargetMachine->createDataLayout());
+
+  std::error_code EC;
+  llvm::raw_fd_ostream dest("output.o", EC, llvm::sys::fs::OF_None);
+
+  if (EC) {
+    llvm::errs() << "Could not open file: " << EC.message();
+    return;
+  }
+
+  llvm::LoopAnalysisManager LAM;
+  llvm::FunctionAnalysisManager FAM;
+  llvm::CGSCCAnalysisManager CGAM;
+  llvm::ModuleAnalysisManager MAM;
+
+  llvm::PassBuilder PB(TargetMachine);
+
+  PB.registerModuleAnalyses(MAM);
+  PB.registerCGSCCAnalyses(CGAM);
+  PB.registerFunctionAnalyses(FAM);
+  PB.registerLoopAnalyses(LAM);
+  PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
+
+  llvm::ModulePassManager MPM =
+      PB.buildPerModuleDefaultPipeline(llvm::OptimizationLevel::O0);
+
+  MPM.run(*TheModule, MAM);
+
+  llvm::legacy::PassManager CodeGenPass;
+  if (TargetMachine->addPassesToEmitFile(CodeGenPass, dest, nullptr,
+                                         llvm::CodeGenFileType::ObjectFile)) {
+    llvm::errs() << "TargetMachine can't emit a file of this type";
+    return;
+  }
+
+  CodeGenPass.run(*TheModule);
+  dest.flush();
+}
+
+void link(const std::string &objectFile, const std::string &executableName) {
+  std::string command = "clang " + objectFile + " -o " + executableName;
+
+  std::cout << "Linking executable: " << executableName << "..." << std::endl;
+  int result = std::system(command.c_str());
+
+  if (result != 0) {
+    throw std::runtime_error("Linking failed with exit code " +
+                             std::to_string(result));
+  }
+
+  std::cout << "Successfully generated " << executableName << std::endl;
+}
 
 void SwaCompiler::Run(const std::string &_source) {
   llvm::InitializeNativeTarget();
@@ -19,6 +90,7 @@ void SwaCompiler::Run(const std::string &_source) {
   auto program = parser.parseProgram();
   auto st = SymbolTable();
   program->Codegen(context, m, builder, st);
+  // m->print(llvm::errs(), nullptr);
 
   llvm::ExecutionEngine *engine = llvm::EngineBuilder(std::move(m)).create();
   auto Fn = engine->FindFunctionNamed("main");
@@ -26,17 +98,32 @@ void SwaCompiler::Run(const std::string &_source) {
 }
 
 void SwaCompiler::Build(const std::string &_source) {
+  llvm::InitializeAllTargetInfos();
   llvm::InitializeNativeTarget();
+  llvm::InitializeAllTargetMCs();
   llvm::InitializeNativeTargetAsmPrinter();
   llvm::InitializeNativeTargetAsmParser();
+  auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+
+  std::string Error;
+  auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
+
+  auto CPU = "generic";
+  auto Features = "";
+  llvm::TargetOptions opt;
+  auto RM = llvm::Reloc::Model::PIC_;
+  auto TargetMachine =
+      Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
 
   Lexer l(_source, KEYWORDS_ENGLISH);
   Parser parser(l.tokenize());
   auto program = parser.parseProgram();
   auto st = SymbolTable();
   program->Codegen(context, m, builder, st);
+  // m->print(llvm::errs(), nullptr);
 
-  llvm::WriteBitcodeToFile(*m, llvm::outs());
+  emitObjectFile(m.get(), TargetMachine);
+  link("output.o", "output");
 }
 
 // void SwaCompiler::Test(const std::string &_source) {}
--- src/swa.cpp
+++ src/swa.cpp
@@ -1,15 +1,43 @@
 #include <compiler/compiler.h>
 #include <string>
 
-int main() {
+int main(int argc, char *argv[]) {
   std::string program = R"(
     dialect:english;
-    main() int { 
-      print("Nangadef Pathe");
+    start() int {
+      let x int := 3;
+      print("x = %d", x);
     }
   )";
   SwaCompiler swa = SwaCompiler();
-  swa.Run(program);
+  if (argc < 2) {
+    std::cerr << "Usage: mytool <subcommand> [options]\n";
+    return 1;
+  }
+
+  std::string subcommand = argv[1];
+
+  if (subcommand == "build") {
+    bool release = false;
+    // Start from index 2 to skip program name and subcommand
+    for (int i = 2; i < argc; ++i) {
+      std::string arg = argv[i];
+      if (arg == "--release") {
+        release = true;
+      }
+    }
+
+    std::cout << "Building in " << (release ? "release" : "debug")
+              << " mode...\n";
+
+    swa.Build(program);
+  } else if (subcommand == "run") {
+    std::cout << "Running project...\n";
+    swa.Run(program);
+  } else {
+    std::cerr << "Unknown subcommand: " << subcommand << "\n";
+    return 1;
+  }
 
   return 0;
 }
