d7caf8aaa5f11e16e7387753e48e45f675193b0c
pathe <pathe.sene@gmail.com>
2026-05-16 09:08:15 +0000

    feat: cleanup build script

--- build.sh
+++ build.sh
@@ -2,16 +2,23 @@
 
 set -euxo pipefail
 
- LLVM_INSTALL_DIR=/usr/include/llvm/ 
- 
+ LLVM_INSTALL_DIR=/usr/include/llvm/
+
  cmake -DLLVM_INSTALL_DIR=$LLVM_INSTALL_DIR \
        -DCMAKE_C_COMPILER=clang-21 \
        -DCMAKE_CXX_COMPILER=clang++ \
        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
        -DLLVM_DIR=/usr/lib/llvm-21/lib/cmake/llvm \
        -G Ninja -B build .
- 
+
  cd build
- cmake --build .
- 
+ cmake --build . --target compiler_tests
+
  ./compiler_tests
+
+
+ cmake --build . --target swa
+
+./swa run
+./swa build
+./swa test
